b6b28e9c6f85848a813e40cbe09d0e6ae9950e5b
tonkhao <chananchida_tha@cmu.ac.th>
2023-03-13 22:12:22 +0700

    cp

--- lab25_3.cpp
+++ lab25_3.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
-
+#include<set>
 using namespace std;
 
+int count(int a[],int N){
+	set<int> b ;
+	for (int i = 0; i < N; i++)
+	{
+		b.insert(a[i]);
+	}
+	return b.size();
+}
 //Write function count() here.
+
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
